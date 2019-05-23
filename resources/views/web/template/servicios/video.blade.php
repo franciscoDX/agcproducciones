a9bb99f4ed9da337fc4bd2cc90a4db3743086947
Francisco Romero <fran6602@gmail.com>
2019-05-23 11:50:38 -0500

    modificaciones pruebas

--- resources/views/web/template/servicios/video.blade.php
+++ resources/views/web/template/servicios/video.blade.php
@@ -13,7 +13,7 @@
 			</div>
 		</div>
 		<div class="col-sm-12 pb-5">
-			<h2>Titulo Video</h2>
+			<h2>Tìtulo Video</h2>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud rcitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
